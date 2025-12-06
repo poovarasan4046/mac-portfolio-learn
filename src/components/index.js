a671be5740681c06923c3c38793295ae2442e797
poovarasan4046 <poovarasanpoovarasan4046@gmail.com>
2025-12-06 14:18:20 +0530

    implement dock

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Welcome from "#components/Welcome";
 import Navbar from "#components/Navbar";
+import { Dock } from "#components/Dock";
 
-export { Welcome, Navbar };
+export { Welcome, Navbar, Dock };
